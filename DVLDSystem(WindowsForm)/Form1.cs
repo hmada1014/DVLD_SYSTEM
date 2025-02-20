b42c57a42d961a781f40b85c536cc5d621c1dd47
Mohammed Khaled <modealkwehe@gmail.com>
2025-02-20 02:54:54 +0200

    add Form1_Load

--- DVLDSystem(WindowsForm)/Form1.cs
+++ DVLDSystem(WindowsForm)/Form1.cs
@@ -16,5 +16,10 @@ namespace DVLDSystem_WindowsForm_
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
