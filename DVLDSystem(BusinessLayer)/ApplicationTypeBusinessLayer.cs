f40f5035ea00c3ea5b1a1db18918962f184502e8
Mohammed Khaled <modealkwehe@gmail.com>
2025-02-24 01:01:02 +0200

    Add class clsApplicationType

---
+++ DVLDSystem(BusinessLayer)/ApplicationTypeBusinessLayer.cs
@@ -0,0 +1,97 @@
+﻿using DVLDSystem_DataAccessLayer_;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLDSystem_BusinessLayer_
+{
+    public class clsApplicationType
+    {
+        public enum enApplicationTypeMode { Add = 0, Update = 1 }
+        public enApplicationTypeMode Mode = enApplicationTypeMode.Add;
+
+        public int ApplicationTypeID { get; set; }
+        public string ApplicationTypeTitle { get; set; }
+        public decimal ApplicationFees { get; set; }
+
+
+        public clsApplicationType()
+
+        {
+            this.ApplicationTypeID = -1;
+            this.ApplicationTypeTitle = string.Empty;
+            this.ApplicationFees = -1;
+
+            this.Mode = enApplicationTypeMode.Add;
+        }
+
+        private clsApplicationType(int ApplicationTypeID, string ApplicationTypeTitle, decimal ApplicationFees)
+        {
+            this.ApplicationTypeID = ApplicationTypeID;
+            this.ApplicationTypeTitle = ApplicationTypeTitle;
+            this.ApplicationFees = ApplicationFees;
+
+            this.Mode = enApplicationTypeMode.Update;
+        }
+        public static clsApplicationType Find(int ApplicationTypeID)
+        {
+            decimal ApplicationFees = -1;
+            string ApplicationTypeTitle = string.Empty;
+
+            if (clsApplicationTypeDataAccessLayer.GetApplicationTypeIDInfoByApplicationTypeID(ApplicationTypeID, ref ApplicationTypeTitle, ref ApplicationFees))
+            {
+                return new clsApplicationType(ApplicationTypeID, ApplicationTypeTitle, ApplicationFees);
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public static bool IsApplicationTypeExist(int ApplicationTypeID)
+        {
+            return clsApplicationTypeDataAccessLayer.IsApplicationTypeIDExist(ApplicationTypeID);
+        }
+        private bool _AddNewApplicationType()
+        {
+            this.ApplicationTypeID = clsApplicationTypeDataAccessLayer.AddNewApplicationTypeID(this.ApplicationTypeTitle, this.ApplicationFees);
+            return (this.ApplicationTypeID != -1);
+        }
+        private bool _UpdateApplicationType()
+        {
+            return clsApplicationTypeDataAccessLayer.UpdateApplicationTypeIDByApplicationTypeID(this.ApplicationTypeID, this.ApplicationTypeTitle, this.ApplicationFees);
+        }
+        public static bool DeleteApplicationType(int ApplicationTypeID)
+        {
+            return clsApplicationDataAccessLayer.DeleteApplicationByApplicationID(ApplicationTypeID);
+        }
+        public static DataView GetAllApplicationTypes()
+        {
+            return clsApplicationTypeDataAccessLayer.GetAllApplicationTypeID();
+        }
+        public bool Save()
+        {
+            switch (this.Mode)
+            {
+                case enApplicationTypeMode.Add:
+                    if (_AddNewApplicationType())
+                    {
+                        this.Mode = enApplicationTypeMode.Update;
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                case enApplicationTypeMode.Update:
+                    return _UpdateApplicationType();
+            }
+            return false;
+        }
+    }
+
+
+
+}
