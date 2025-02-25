5c2676ac41bf9906f84695492423ecd5d3f58440
Mohammed Khaled <modealkwehe@gmail.com>
2025-02-25 03:23:31 +0200

    add clsDragForm to folder Drag Form

---
+++ DVLDSystem(WindowsForm)/Drag Form/clsDragForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentalVehical_WindowsForm
+{
+    public class clsDragForm
+    {
+        //Drag Form
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        public extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        public extern static void SendMessage(System.IntPtr hWnd, int wMsg = 0xA1 , int wParam = 0x2 , int lParam =0);
+    }
+}
