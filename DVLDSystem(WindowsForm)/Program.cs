15d525516122d010a91f1435db8a6d7648fa8261
Mohammed Khaled <modealkwehe@gmail.com>
2025-02-25 03:33:42 +0200

    Application.Run(new frmLoginScreen())

--- DVLDSystem(WindowsForm)/Program.cs
+++ DVLDSystem(WindowsForm)/Program.cs
@@ -16,7 +16,8 @@ namespace DVLDSystem_WindowsForm_
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //Application.Run(new Form1());
+            Application.Run(new frmLoginScreen());
         }
     }
 }
