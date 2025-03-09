81148f9a52e622ce3486e0ee71ec2f1088591edc
Mohammed Khaled <modealkwehe@gmail.com>
2025-03-09 05:49:44 +0200

    Add form People

---
+++ DVLDSystem(WindowsForm)/frmPeople.cs
@@ -0,0 +1,33 @@
+﻿using DVLDSystem_BusinessLayer_;
+using DVLDSystem_WindowsForm_.User_Control;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLDSystem_WindowsForm_
+{
+    public partial class frmPeople : Form
+    {
+        public frmPeople()
+        {
+            InitializeComponent();
+        }
+
+        
+        
+
+        private void frmPeople_Load(object sender, EventArgs e)
+        {
+            //ucTemplateDGVAndSearch.RefreshDGV(clsPerson.GetAllPersons());
+
+            ucTemplateDGVAndSearch.RefreshDGV(clsPerson.GetAllPersons());
+
+        }
+    }
+}
