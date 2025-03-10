54ffe175eb3d1ca567d73ac78afccfc68c5bc5e1
Mohammed Khaled <modealkwehe@gmail.com>
2025-03-10 02:19:02 +0200

    add new User Form

---
+++ DVLDSystem(WindowsForm)/User/frmUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLDSystem_WindowsForm_.User
+{
+    public partial class frmUser : Form
+    {
+        public frmUser()
+        {
+            InitializeComponent();
+        }
+    }
+}
