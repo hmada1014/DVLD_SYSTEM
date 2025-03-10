f0e2355c016cb0ca601f4a5672eb2fc24eae83a6
Mohammed Khaled <modealkwehe@gmail.com>
2025-03-10 02:42:35 +0200

    add new from AddEditPeople

---
+++ DVLDSystem(WindowsForm)/People/frmAddEditPeople.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLDSystem_WindowsForm_.People
+{
+    public partial class frmAddEditPeople : Form
+    {
+        public frmAddEditPeople()
+        {
+            InitializeComponent();
+        }
+    }
+}
