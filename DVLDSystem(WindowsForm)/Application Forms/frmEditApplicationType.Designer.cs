81351852ecdccc81e60942d4d421bdadb983be54
Mohammed Khaled <modealkwehe@gmail.com>
2025-04-20 16:23:38 +0200

    add new form frmManagApplicationTypes ,frmEditApplicationType

---
+++ DVLDSystem(WindowsForm)/Application Forms/frmEditApplicationType.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace DVLDSystem_WindowsForm_.Application_Forms
+{
+    partial class frmEditApplicationType
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblUpdateApplicationTitle = new System.Windows.Forms.Label();
+            this.lblIDTitle = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblFeesTitle = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.txtApplicationTypeTitle = new Guna.UI2.WinForms.Guna2TextBox();
+            this.txtApplicationFees = new Guna.UI2.WinForms.Guna2TextBox();
+            this.lblID = new System.Windows.Forms.Label();
+            this.btnClose = new Guna.UI2.WinForms.Guna2Button();
+            this.btnSave = new Guna.UI2.WinForms.Guna2Button();
+            this.ep1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ep1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblUpdateApplicationTitle
+            // 
+            this.lblUpdateApplicationTitle.AutoSize = true;
+            this.lblUpdateApplicationTitle.Font = new System.Drawing.Font("Souvenir Lt BT", 25F, System.Drawing.FontStyle.Bold);
+            this.lblUpdateApplicationTitle.ForeColor = System.Drawing.Color.Maroon;
+            this.lblUpdateApplicationTitle.Location = new System.Drawing.Point(21, 34);
+            this.lblUpdateApplicationTitle.Name = "lblUpdateApplicationTitle";
+            this.lblUpdateApplicationTitle.Size = new System.Drawing.Size(431, 42);
+            this.lblUpdateApplicationTitle.TabIndex = 0;
+            this.lblUpdateApplicationTitle.Text = "Update Application Type";
+            // 
+            // lblIDTitle
+            // 
+            this.lblIDTitle.AutoSize = true;
+            this.lblIDTitle.Font = new System.Drawing.Font("Souvenir Lt BT", 12F, System.Drawing.FontStyle.Bold);
+            this.lblIDTitle.Location = new System.Drawing.Point(28, 116);
+            this.lblIDTitle.Name = "lblIDTitle";
+            this.lblIDTitle.Size = new System.Drawing.Size(33, 20);
+            this.lblIDTitle.TabIndex = 1;
+            this.lblIDTitle.Text = "ID:";
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Souvenir Lt BT", 12F, System.Drawing.FontStyle.Bold);
+            this.lblTitle.Location = new System.Drawing.Point(12, 153);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(49, 20);
+            this.lblTitle.TabIndex = 2;
+            this.lblTitle.Text = "Title:";
+            // 
+            // lblFeesTitle
+            // 
+            this.lblFeesTitle.AutoSize = true;
+            this.lblFeesTitle.Font = new System.Drawing.Font("Souvenir Lt BT", 12F, System.Drawing.FontStyle.Bold);
+            this.lblFeesTitle.Location = new System.Drawing.Point(11, 186);
+            this.lblFeesTitle.Name = "lblFeesTitle";
+            this.lblFeesTitle.Size = new System.Drawing.Size(50, 20);
+            this.lblFeesTitle.TabIndex = 3;
+            this.lblFeesTitle.Text = "Fees:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Image = global::DVLDSystem_WindowsForm_.Properties.Resources.ApplicationTitle;
+            this.pictureBox1.Location = new System.Drawing.Point(76, 151);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(25, 25);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox2.Image = global::DVLDSystem_WindowsForm_.Properties.Resources.money_32;
+            this.pictureBox2.Location = new System.Drawing.Point(76, 184);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(25, 25);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // txtApplicationTypeTitle
+            // 
+            this.txtApplicationTypeTitle.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtApplicationTypeTitle.DefaultText = "";
+            this.txtApplicationTypeTitle.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtApplicationTypeTitle.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtApplicationTypeTitle.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtApplicationTypeTitle.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtApplicationTypeTitle.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtApplicationTypeTitle.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txtApplicationTypeTitle.ForeColor = System.Drawing.Color.Black;
+            this.txtApplicationTypeTitle.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtApplicationTypeTitle.Location = new System.Drawing.Point(122, 149);
+            this.txtApplicationTypeTitle.Name = "txtApplicationTypeTitle";
+            this.txtApplicationTypeTitle.PasswordChar = '\0';
+            this.txtApplicationTypeTitle.PlaceholderText = "";
+            this.txtApplicationTypeTitle.SelectedText = "";
+            this.txtApplicationTypeTitle.Size = new System.Drawing.Size(330, 28);
+            this.txtApplicationTypeTitle.TabIndex = 6;
+            this.txtApplicationTypeTitle.Validating += new System.ComponentModel.CancelEventHandler(this.txtApplicationTypeTitle_Validating);
+            // 
+            // txtApplicationFees
+            // 
+            this.txtApplicationFees.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtApplicationFees.DefaultText = "";
+            this.txtApplicationFees.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtApplicationFees.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtApplicationFees.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtApplicationFees.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtApplicationFees.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtApplicationFees.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txtApplicationFees.ForeColor = System.Drawing.Color.Black;
+            this.txtApplicationFees.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtApplicationFees.Location = new System.Drawing.Point(122, 182);
+            this.txtApplicationFees.Name = "txtApplicationFees";
+            this.txtApplicationFees.PasswordChar = '\0';
+            this.txtApplicationFees.PlaceholderText = "";
+            this.txtApplicationFees.SelectedText = "";
+            this.txtApplicationFees.Size = new System.Drawing.Size(330, 28);
+            this.txtApplicationFees.TabIndex = 7;
+            this.txtApplicationFees.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtApplicationFees_KeyPress);
+            this.txtApplicationFees.Validating += new System.ComponentModel.CancelEventHandler(this.txtApplicationTypeTitle_Validating);
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Font = new System.Drawing.Font("Souvenir Lt BT", 12F, System.Drawing.FontStyle.Bold);
+            this.lblID.Location = new System.Drawing.Point(122, 116);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(36, 20);
+            this.lblID.TabIndex = 8;
+            this.lblID.Text = "???";
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.SystemColors.Control;
+            this.btnClose.BorderThickness = 1;
+            this.btnClose.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnClose.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnClose.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnClose.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnClose.FillColor = System.Drawing.SystemColors.Control;
+            this.btnClose.Font = new System.Drawing.Font("Tahoma", 15F);
+            this.btnClose.ForeColor = System.Drawing.Color.Black;
+            this.btnClose.Image = global::DVLDSystem_WindowsForm_.Properties.Resources.Close_32;
+            this.btnClose.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.btnClose.ImageSize = new System.Drawing.Size(30, 30);
+            this.btnClose.Location = new System.Drawing.Point(190, 240);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(128, 38);
+            this.btnClose.TabIndex = 40;
+            this.btnClose.Text = "close";
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.SystemColors.Control;
+            this.btnSave.BorderThickness = 1;
+            this.btnSave.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnSave.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnSave.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnSave.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnSave.FillColor = System.Drawing.SystemColors.Control;
+            this.btnSave.Font = new System.Drawing.Font("Tahoma", 15F);
+            this.btnSave.ForeColor = System.Drawing.Color.Black;
+            this.btnSave.Image = global::DVLDSystem_WindowsForm_.Properties.Resources.Save_32;
+            this.btnSave.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.btnSave.ImageSize = new System.Drawing.Size(30, 30);
+            this.btnSave.Location = new System.Drawing.Point(324, 240);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(128, 38);
+            this.btnSave.TabIndex = 39;
+            this.btnSave.Text = "Save";
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // ep1
+            // 
+            this.ep1.ContainerControl = this;
+            // 
+            // frmEditApplicationType
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 290);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.txtApplicationFees);
+            this.Controls.Add(this.txtApplicationTypeTitle);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblFeesTitle);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.lblIDTitle);
+            this.Controls.Add(this.lblUpdateApplicationTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmEditApplicationType";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "frmEditApplicationType";
+            this.Load += new System.EventHandler(this.frmEditApplicationType_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ep1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblUpdateApplicationTitle;
+        private System.Windows.Forms.Label lblIDTitle;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblFeesTitle;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private Guna.UI2.WinForms.Guna2TextBox txtApplicationTypeTitle;
+        private Guna.UI2.WinForms.Guna2TextBox txtApplicationFees;
+        private System.Windows.Forms.Label lblID;
+        private Guna.UI2.WinForms.Guna2Button btnClose;
+        private Guna.UI2.WinForms.Guna2Button btnSave;
+        private System.Windows.Forms.ErrorProvider ep1;
+    }
+}
---
+++ DVLDSystem(WindowsForm)/Application Forms/frmEditApplicationType.cs
@@ -0,0 +1,161 @@
+﻿using DVLDSystem_BusinessLayer_;
+using Guna.UI2.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace DVLDSystem_WindowsForm_.Application_Forms
+{
+    public partial class frmEditApplicationType : Form
+    {
+        private enum enMode { Update =1 ,Empty}
+        private enMode _Mode = enMode.Update;
+        private int _ID;
+
+        private clsApplicationTypes _ApplicationTypes;
+        public frmEditApplicationType(int iD)
+        {
+            InitializeComponent();
+            _ID = iD;
+            
+            if (_ID <= 0)
+            {
+                _Mode = enMode.Empty;
+            }
+            else
+            {
+                _Mode = enMode.Update;
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        
+
+        private void _LoadData()
+        {
+            if (_Mode == enMode.Empty)
+            {
+                return;
+            }
+
+            _ApplicationTypes = clsApplicationTypes.Find(_ID);
+
+            if (_ApplicationTypes == null)
+            {
+                MessageBox.Show("this page will colse becuese no Application Types found ", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Close();
+                return;
+            }
+
+            if (_Mode == enMode.Update)
+            {
+                _FillApplicationTypeForm();
+            }
+        }
+
+        private void _FillApplicationTypeForm()
+        {
+            lblID.Text = _ApplicationTypes.ApplicationTypeID.ToString();
+            txtApplicationTypeTitle.Text = _ApplicationTypes.ApplicationTypeTitle;
+            txtApplicationFees.Text = Convert.ToSingle( _ApplicationTypes.ApplicationFees).ToString();
+        }
+        private void frmEditApplicationType_Load(object sender, EventArgs e)
+        {
+            _LoadData();
+        }
+
+        private bool IsEmpty(object sender)
+        {
+            Guna2TextBox txt = sender as Guna2TextBox;
+
+            if (string.IsNullOrWhiteSpace(txt.Text.Trim()))
+            {
+                return false;
+            }
+            
+            return true;
+
+        }
+
+        private bool IsValidForm()
+        {
+            bool Isvalid = true;
+
+            Isvalid &= IsEmpty(txtApplicationFees);
+            Isvalid &= IsEmpty(txtApplicationTypeTitle);
+
+            return Isvalid;
+        }
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+            if (!IsValidForm())
+            {
+                MessageBox.Show("Please fill all required fields Correctly before saving.", "Validation Failed", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            _FillApplicationType();
+            _SaveApplicationType();
+        }
+
+        private void _SaveApplicationType()
+        {
+            if (_ApplicationTypes.Save())
+            {
+                MessageBox.Show("Application Type was saved Successfully", "Saved", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                _Mode = enMode.Update;
+                lblID.Text = _ApplicationTypes.ApplicationTypeID.ToString();
+                txtApplicationTypeTitle.Text = _ApplicationTypes.ApplicationTypeTitle.ToString();
+                txtApplicationFees.Text = _ApplicationTypes.ApplicationFees.ToString();
+
+            }
+            else
+            {
+                MessageBox.Show("Application Type was not saved Successfully", "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void _FillApplicationType()
+        {
+            _ApplicationTypes.ApplicationTypeTitle = txtApplicationTypeTitle.Text;
+            _ApplicationTypes.ApplicationFees = Convert.ToDecimal(txtApplicationFees.Text);
+        }
+
+        private void txtApplicationFees_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && e.KeyChar != '.')
+            {
+                e.Handled = true; // Prevent input
+            }
+
+            // Ensure only one decimal point is allowed
+            if (e.KeyChar == '.' && (sender as Guna.UI2.WinForms.Guna2TextBox).Text.Contains("."))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtApplicationTypeTitle_Validating(object sender, CancelEventArgs e)
+        {
+            Guna2TextBox txt = sender as Guna2TextBox;
+            if (string.IsNullOrWhiteSpace(txt.Text.Trim()))
+            {
+                ep1.SetError(txt , "Required to fill.");
+            }
+            else
+            {
+                ep1.SetError(txt, string.Empty);
+            }
+        }
+    }
+}
