048c3386a9bd9806930970d254b2941d08c340b0
Mohammed Khaled <modealkwehe@gmail.com>
2025-04-21 16:51:09 +0200

    add new functions

--- DVLDSystem(WindowsForm)/User Control/ucTemplateDGV.Designer.cs
+++ DVLDSystem(WindowsForm)/User Control/ucTemplateDGV.Designer.cs
@@ -28,32 +28,117 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.dgvShowList = new System.Windows.Forms.DataGridView();
+            this.cmsGeneral = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.lblRrecords = new System.Windows.Forms.Label();
+            this.lblRecordsTitle = new System.Windows.Forms.Label();
+            this.TSMEditApplicationType = new System.Windows.Forms.ToolStripMenuItem();
+            this.TSMManageTestTypes = new System.Windows.Forms.ToolStripMenuItem();
+            this.TSMRefresh = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.dgvShowList)).BeginInit();
+            this.cmsGeneral.SuspendLayout();
             this.SuspendLayout();
             // 
             // dgvShowList
             // 
+            this.dgvShowList.AllowUserToAddRows = false;
+            this.dgvShowList.AllowUserToDeleteRows = false;
+            this.dgvShowList.AllowUserToResizeColumns = false;
+            this.dgvShowList.AllowUserToResizeRows = false;
+            this.dgvShowList.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.dgvShowList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvShowList.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.dgvShowList.Location = new System.Drawing.Point(0, 88);
+            this.dgvShowList.ContextMenuStrip = this.cmsGeneral;
+            this.dgvShowList.Location = new System.Drawing.Point(0, 26);
             this.dgvShowList.Name = "dgvShowList";
-            this.dgvShowList.Size = new System.Drawing.Size(640, 290);
+            this.dgvShowList.ReadOnly = true;
+            this.dgvShowList.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvShowList.Size = new System.Drawing.Size(640, 330);
             this.dgvShowList.TabIndex = 6;
+            // 
+            // cmsGeneral
+            // 
+            this.cmsGeneral.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.TSMEditApplicationType,
+            this.TSMManageTestTypes,
+            this.TSMRefresh});
+            this.cmsGeneral.Name = "cmsGeneral";
+            this.cmsGeneral.Size = new System.Drawing.Size(202, 140);
+            this.cmsGeneral.Opening += new System.ComponentModel.CancelEventHandler(this.cmsGeneral_Opening);
+            // 
+            // lblRrecords
+            // 
+            this.lblRrecords.AutoSize = true;
+            this.lblRrecords.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.lblRrecords.Location = new System.Drawing.Point(114, 361);
+            this.lblRrecords.Name = "lblRrecords";
+            this.lblRrecords.Size = new System.Drawing.Size(18, 19);
+            this.lblRrecords.TabIndex = 10;
+            this.lblRrecords.Text = "0";
+            // 
+            // lblRecordsTitle
+            // 
+            this.lblRecordsTitle.AutoSize = true;
+            this.lblRecordsTitle.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.lblRecordsTitle.Location = new System.Drawing.Point(17, 360);
+            this.lblRecordsTitle.Name = "lblRecordsTitle";
+            this.lblRecordsTitle.Size = new System.Drawing.Size(88, 19);
+            this.lblRecordsTitle.TabIndex = 9;
+            this.lblRecordsTitle.Text = "#Records :";
+            // 
+            // TSMEditApplicationType
+            // 
+            this.TSMEditApplicationType.Image = global::DVLDSystem_WindowsForm_.Properties.Resources.edit_32;
+            this.TSMEditApplicationType.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.TSMEditApplicationType.Name = "TSMEditApplicationType";
+            this.TSMEditApplicationType.Size = new System.Drawing.Size(201, 38);
+            this.TSMEditApplicationType.Text = "Edit Application Type";
+            this.TSMEditApplicationType.Visible = false;
+            this.TSMEditApplicationType.Click += new System.EventHandler(this.cmsGeneral_Click);
+            // 
+            // TSMManageTestTypes
+            // 
+            this.TSMManageTestTypes.Image = global::DVLDSystem_WindowsForm_.Properties.Resources.edit_32;
+            this.TSMManageTestTypes.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.TSMManageTestTypes.Name = "TSMManageTestTypes";
+            this.TSMManageTestTypes.Size = new System.Drawing.Size(201, 38);
+            this.TSMManageTestTypes.Text = "Edit Test Type";
+            this.TSMManageTestTypes.Visible = false;
+            this.TSMManageTestTypes.Click += new System.EventHandler(this.cmsGeneral_Click);
+            // 
+            // TSMRefresh
+            // 
+            this.TSMRefresh.Image = global::DVLDSystem_WindowsForm_.Properties.Resources.Refresh_32;
+            this.TSMRefresh.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.TSMRefresh.Name = "TSMRefresh";
+            this.TSMRefresh.Size = new System.Drawing.Size(201, 38);
+            this.TSMRefresh.Text = "Refresh";
+            this.TSMRefresh.Click += new System.EventHandler(this.TSMRefresh_Click);
             // 
             // ucTemplateDGV
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lblRrecords);
+            this.Controls.Add(this.lblRecordsTitle);
             this.Controls.Add(this.dgvShowList);
             this.Name = "ucTemplateDGV";
-            this.Size = new System.Drawing.Size(640, 378);
+            this.Size = new System.Drawing.Size(640, 386);
             ((System.ComponentModel.ISupportInitialize)(this.dgvShowList)).EndInit();
+            this.cmsGeneral.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.DataGridView dgvShowList;
+        private System.Windows.Forms.Label lblRrecords;
+        private System.Windows.Forms.Label lblRecordsTitle;
+        private System.Windows.Forms.ContextMenuStrip cmsGeneral;
+        private System.Windows.Forms.ToolStripMenuItem TSMEditApplicationType;
+        private System.Windows.Forms.ToolStripMenuItem TSMRefresh;
+        private System.Windows.Forms.ToolStripMenuItem TSMManageTestTypes;
     }
 }
--- DVLDSystem(WindowsForm)/User Control/ucTemplateDGV.cs
+++ DVLDSystem(WindowsForm)/User Control/ucTemplateDGV.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using DVLDSystem_BusinessLayer_;
+using DVLDSystem_WindowsForm_.Application_Forms;
+using DVLDSystem_WindowsForm_.People;
+using DVLDSystem_WindowsForm_.User;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -6,15 +10,196 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Configuration;
 using System.Windows.Forms;
 
 namespace DVLDSystem_WindowsForm_.User_Control
 {
     public partial class ucTemplateDGV : UserControl
     {
+        private enum enModeUC { ManageApplicationTypes = 1, ManageTestTypes = 2, Empty }
+        enModeUC _enMode = enModeUC.Empty;
+        private string _FormName;
         public ucTemplateDGV()
         {
             InitializeComponent();
+        }
+        public ucTemplateDGV(string FormName)
+        {
+            InitializeComponent();
+
+            _FormName = FormName;
+            switch (_FormName)
+            {
+                case "frmManageApplicationTypes":
+                    TSMEditApplicationType.Visible = true;
+                    _enMode = enModeUC.ManageApplicationTypes; 
+                    break;
+                case "frmManageTestTypes":
+                    TSMManageTestTypes.Visible = true;
+                    _enMode = enModeUC.ManageTestTypes;
+                    break;
+            }
+        }
+
+
+        /*########### Expose Properties of Controls in UserControl #######*/
+
+        [Browsable(true)]
+        [Category("Misc Controls Properties")]
+        public DataGridViewAutoSizeColumnsMode dgvSizeColumnsMode
+        {
+            get { return dgvShowList.AutoSizeColumnsMode; }
+            set { dgvShowList.AutoSizeColumnsMode = value; }
+        }
+        [Browsable(true)]
+        [Category("Misc Controls Properties")]
+        public string Rdcords
+        {
+            get { return lblRrecords.Text; }
+        }
+        [Browsable(true)]
+        [Category("Misc Controls Properties")]
+        public Color dgvColor
+        {
+            get { return dgvShowList.BackgroundColor; }
+            set { dgvShowList.BackgroundColor = value; }
+        }
+
+        //############################ UI Data Helper ######################
+        public void RefreshDGV(object DataSours)
+        {
+            DataView dv = _ConvertToDataView(DataSours);
+            if (dv == null) return;
+
+            switch (_enMode)
+            {
+                case enModeUC.ManageApplicationTypes:
+                     dgvShowList.DataSource = dv;
+                    _UpdateManageApplicationTypesColumnHeaders();
+                    break;
+                case enModeUC.ManageTestTypes:
+                    dgvShowList.DataSource = dv;
+                    _ResizeDgvForManageTestType();
+                    _UpdateManageTestTypeColumnHeaders();
+                    break;
+            }
+
+            lblRrecords.Text = dgvShowList.RowCount.ToString();
+        }
+        private DataView _ConvertToDataView(object DataSours)
+        {
+            if (DataSours is DataView)
+            {
+                return (DataView)DataSours;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        private void _SafeHeaderUpdate(string ColumnName, string headerText)
+        {
+            if (dgvShowList.Columns.Contains(ColumnName))
+            {
+                dgvShowList.Columns[ColumnName].HeaderText = headerText;
+            }
+        }
+        private void _UpdateManageApplicationTypesColumnHeaders()
+        {
+            _SafeHeaderUpdate("ApplicationTypeID", "ID");
+            _SafeHeaderUpdate("ApplicationTypeTitle", "Title");
+            _SafeHeaderUpdate("ApplicationFees", "Fees");
+        }
+        private void _UpdateManageTestTypeColumnHeaders()
+        {
+            _SafeHeaderUpdate("TestTypeID", "ID");
+            _SafeHeaderUpdate("TestTypeTitle", "Title");
+            _SafeHeaderUpdate("TestTypeDescription", "Description");
+            _SafeHeaderUpdate("TestTypeFees", "Fees");
+        }
+        private void _ResizeDgvForManageTestType()
+        {
+            dgvShowList.Columns["TestTypeDescription"].Width = 300;
+            dgvShowList.Columns["TestTypeTitle"].Width = 135;
+            dgvShowList.Columns["TestTypeID"].Width = 50;
+        }
+
+        /*########################### cmsGeneralMenu ################################*/
+
+        //-------------------_EditApplicationType --------------------------------
+        private void _EditApplicationType()
+        {
+            if (int.TryParse(dgvShowList.CurrentRow.Cells["ApplicationTypeID"].Value.ToString(), out int ID))
+            {
+                frmEditApplicationType EditapplicationType = new frmEditApplicationType(ID);
+                EditapplicationType.ShowDialog();
+                TSMRefresh_Click(null,null);
+            }
+            else
+            {
+                MessageBox.Show("Application Type Not Found to Edit.", "Failed");
+            }
+        }
+        //-------------------_EditTestType ---------------------------------------
+        private void _EditTestType()
+        {
+            if (int.TryParse(dgvShowList.CurrentRow.Cells["TestTypeID"].Value.ToString(), out int ID))
+            {
+                frmEditTestType EditTestTypes = new frmEditTestType(ID);
+                EditTestTypes.ShowDialog();
+                TSMRefresh_Click(null, null);
+            }
+            else
+            {
+                MessageBox.Show("User Not Found to Edit.", "warning");
+            }
+        }
+
+        //########################## events #####################################
+        private void cmsGeneral_Opening(object sender, CancelEventArgs e)
+        {
+            if (dgvShowList.Rows.Count == 0)
+            {
+                e.Cancel = true;
+                return;
+            }
+
+            // Cursor.Position current mouse position 
+            // Get mouse position relative to the DataGridView
+            Point clientPoint = dgvShowList.PointToClient(Cursor.Position);
+
+            // Check if the click is on a valid row
+            DataGridView.HitTestInfo hitTest = dgvShowList.HitTest(clientPoint.X, clientPoint.Y);
+            if (hitTest.Type != DataGridViewHitTestType.Cell || hitTest.RowIndex < 0)
+            {
+                e.Cancel = true;
+            }
+        }
+        private void TSMRefresh_Click(object sender, EventArgs e)
+        {
+
+            switch(_enMode)
+            {
+                case enModeUC.ManageApplicationTypes:
+                    RefreshDGV(clsApplicationTypes.GetAllApplicationTypes());
+                    break;
+                case enModeUC.ManageTestTypes:
+                    RefreshDGV(clsTestType.GetAllTestTypeDatas());
+                    break;
+            }
+        }
+        private void cmsGeneral_Click(object sender, EventArgs e)
+        {
+            switch (_enMode)
+            {
+                case enModeUC.ManageApplicationTypes:
+                    _EditApplicationType();
+                    break;
+                case enModeUC.ManageTestTypes:
+                    _EditTestType();
+                    break;
+            }
         }
     }
 }
