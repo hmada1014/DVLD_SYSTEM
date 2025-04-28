0308725f236e61785d998cef3f0bc1712ec91503
Mohammed Khaled <modealkwehe@gmail.com>
2025-04-28 03:18:25 +0300

    add refresh to btnAddLocalDrivingLicenseApplication_Click

---
+++ DVLDSystem(WindowsForm)/Application Forms/frmLDLApplicationDetails.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace DVLDSystem_WindowsForm_.Application_Forms
+{
+    partial class frmLDLApplicationDetails
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ucDrivingLicenseApplicationInfo1 = new DVLDSystem_WindowsForm_.User_Control.ucDrivingLicenseApplicationInfo();
+            this.ucApplicationBasicInfo1 = new DVLDSystem_WindowsForm_.User_Control.ucApplicationBasicInfo();
+            this.btnClose = new Guna.UI2.WinForms.Guna2Button();
+            this.SuspendLayout();
+            // 
+            // ucDrivingLicenseApplicationInfo1
+            // 
+            this.ucDrivingLicenseApplicationInfo1.Location = new System.Drawing.Point(7, 12);
+            this.ucDrivingLicenseApplicationInfo1.Name = "ucDrivingLicenseApplicationInfo1";
+            this.ucDrivingLicenseApplicationInfo1.Size = new System.Drawing.Size(784, 150);
+            this.ucDrivingLicenseApplicationInfo1.TabIndex = 0;
+            // 
+            // ucApplicationBasicInfo1
+            // 
+            this.ucApplicationBasicInfo1.Location = new System.Drawing.Point(7, 187);
+            this.ucApplicationBasicInfo1.Name = "ucApplicationBasicInfo1";
+            this.ucApplicationBasicInfo1.Size = new System.Drawing.Size(784, 248);
+            this.ucApplicationBasicInfo1.TabIndex = 1;
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.SystemColors.Control;
+            this.btnClose.BorderThickness = 1;
+            this.btnClose.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnClose.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnClose.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnClose.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnClose.FillColor = System.Drawing.SystemColors.Control;
+            this.btnClose.Font = new System.Drawing.Font("Tahoma", 15F);
+            this.btnClose.ForeColor = System.Drawing.Color.Black;
+            this.btnClose.Image = global::DVLDSystem_WindowsForm_.Properties.Resources.Close_32;
+            this.btnClose.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.btnClose.ImageSize = new System.Drawing.Size(30, 30);
+            this.btnClose.Location = new System.Drawing.Point(651, 446);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(128, 38);
+            this.btnClose.TabIndex = 45;
+            this.btnClose.Text = "close";
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // frmLDLApplicationDetails
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(792, 505);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.ucApplicationBasicInfo1);
+            this.Controls.Add(this.ucDrivingLicenseApplicationInfo1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmLDLApplicationDetails";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "frmLDLApplicationDetails";
+            this.Load += new System.EventHandler(this.frmLDLApplicationDetails_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private User_Control.ucDrivingLicenseApplicationInfo ucDrivingLicenseApplicationInfo1;
+        private User_Control.ucApplicationBasicInfo ucApplicationBasicInfo1;
+        private Guna.UI2.WinForms.Guna2Button btnClose;
+    }
+}
---
+++ DVLDSystem(WindowsForm)/Application Forms/frmLDLApplicationDetails.cs
@@ -0,0 +1,47 @@
+﻿using DVLDSystem_BusinessLayer_;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLDSystem_WindowsForm_.Application_Forms
+{
+    public partial class frmLDLApplicationDetails : Form
+    {
+        private clsLocalDrivingLicenseApplication _LDLApplication;
+        private int _LDLApplicationID;
+        public frmLDLApplicationDetails(int LDLApplicationID)
+        {
+            InitializeComponent();
+            if (LDLApplicationID > -1)
+            {
+                _LDLApplicationID = LDLApplicationID;   
+            }
+        }
+
+        private void frmLDLApplicationDetails_Load(object sender, EventArgs e)
+        {
+            _LDLApplication = clsLocalDrivingLicenseApplication.Find(_LDLApplicationID);
+            if (_LDLApplication != null)
+            {
+                ucDrivingLicenseApplicationInfo1.LoadDLApplicationInfo(_LDLApplication.LocalDrivingLicenseApplicationID); ;
+                ucApplicationBasicInfo1.LoadApplicationInfo(_LDLApplication.ApplicationID);
+            }
+            else
+            {
+                MessageBox.Show("this form will close");
+                this.Close();
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
